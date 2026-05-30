2b6a42e36f3191d4d50f76fd5af5a459130ec9ad
Marcelo San Martín <ma.sanmartinu@duocuc.cl>
2026-05-30 17:23:52 -0400

    Agrega arquetipo Maven inicial para BFF aplicando patron API Gateway

---
+++ src/main/java/com/rednorte/bff_rednorte/BffRednorteApplication.java
@@ -0,0 +1,13 @@
+package com.rednorte.bff_rednorte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffRednorteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffRednorteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rednorte/bff_rednorte/BffRednorteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rednorte.bff_rednorte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffRednorteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
